1dc7d61b67a62f7a8e374287eb31186e650e13f5
MUrban <manuel.urban@uni-bayreuth.de>
2025-07-10 13:09:31 +0200

    refine gpu algorithm

--- fastnetworkgeneration/sampling_algorithms/PZER.jl
+++ fastnetworkgeneration/sampling_algorithms/PZER.jl
@@ -1,7 +1,5 @@
 using CUDA
-#using BenchmarkTools
 using Statistics
-#using Base.Threads
 
 
 function compute_single_edge_skip(edge_probability::Float64)
@@ -18,7 +16,7 @@ function compute_edge_skips_gpu(
     rng_gen(_) = compute_single_edge_skip(edge_probability)
     skip_values = CUDA.map(rng_gen, skip_values)   
     skip_values = CUDA.accumulate(+, skip_values)
-    
+
     skip_values .+= start_index 
 
     return skip_values
@@ -31,7 +29,7 @@ function gpu_compute_loop(
     edge_probabiltiy::Real,
     chunk_size::Integer
 )
-    @info "Starting GPU loop"
+    @debug "Starting GPU loop"
     current_adj_mat_index = 0
     execution_times = 0
     execution_counts = 0
@@ -48,8 +46,8 @@ function gpu_compute_loop(
 
         put!(buffer, edge_list_h)
         
-        @info "GPU execution time for edge list $((time()-start_time)/1_000_000)"
-        execution_times  += time()-start_time
+        @debug "GPU execution time for edge list $((time()-start_time)/1_000_000)"
+        execution_times  += time() - start_time
         execution_counts += 1
         if edge_list_h[end] > n_nodes^2/2
             @debug "Finishing GPU loop"
@@ -63,17 +61,19 @@ end
 
 
 function cpu_compute_loop!(
-    adj_mat::BitMatrix,
+    network::SimpleNetwork,
     buffer::Channel,
     n_nodes::Int;
     compute_inverse::Bool=false,
 )
-    @info "Starting CPU loop"
+    @debug "Starting CPU loop"
 
     row_index::Int = 1
     row_index_bound = 1
     col_index_bound = 0
 
+    max_n_edges = n_nodes * (n_nodes-1)/2
+    
     execution_times = 0
     execution_counts = 0
 
@@ -81,108 +81,109 @@ function cpu_compute_loop!(
     for edge_list in buffer
         start_time = time()
         for edge_index in edge_list
+
+            # break condition
+            if edge_index > max_n_edges
+                break
+            end
+
             while row_index_bound < edge_index 
-                row_index +=1
+                row_index += 1
                 col_index_bound = row_index_bound 
                 row_index_bound = (row_index^2 + row_index)/2
             end
            
             col_index::Int = edge_index - col_index_bound
-
-            if row_index < n_nodes && col_index <= n_nodes
-                @inbounds adj_mat[row_index+1, col_index] = !compute_inverse
-                @inbounds adj_mat[col_index, row_index+1] = !compute_inverse
-            else
-               break 
-            end
+            
+            insert_edge!(network, (row_index+1, col_index))
+            
         end
 
         execution_counts += 1
         execution_times += time() - start_time
-        @info "CPU execution time for edge list: $((time()-start_time)/1_000_000)"
+        @debug "CPU execution time for edge list: $((time()-start_time)/1_000_000)"
 
 
         if row_index >= n_nodes
             break
         end
     end
-    @info "Finishing CPU loop"
+    @debug "Finishing CPU loop"
     return execution_counts, execution_times
 end
 
 
-function compute_uniform_random_graph_PZER(n_nodes, edge_probability)
+function sample_uniform_random_graph_PZER!(
+    network::SimpleNetwork,
+    n_nodes::Integer, 
+    edge_probability::Real;
+    gpu_chunksize::Integer=1024, 
+    buffersize::Integer=true,
+    verbose=true
+    )
     
+    buffer = Channel{Vector{Integer}}(buffersize)
 
-    chunksize = 1024*4
-    buffer = Channel{Vector{Float64}}(5)
-
-    adj_mat = falses(n_nodes, n_nodes)
-
+    start_time = time()
     local gpu_task, cpu_task
     begin 
-        gpu_task = Base.Threads.@spawn gpu_compute_loop(buffer, n_nodes, edge_probability, chunksize)
-        cpu_task = Base.Threads.@spawn cpu_compute_loop!(adj_mat, buffer, n_nodes)
+        gpu_task = Base.Threads.@spawn gpu_compute_loop(buffer, n_nodes, edge_probability, gpu_chunksize)
+        cpu_task = Base.Threads.@spawn cpu_compute_loop!(network, buffer, n_nodes)
     end
-
+    total_execution_time = time() - start_time
     gpu_counts, gpu_exec_time = fetch(gpu_task)
     cpu_counts, cpu_exec_time = fetch(cpu_task)
 
-    @info "GPU and CPU loop finished"
-    println("GPU executions: $(gpu_counts), average GPU execution time $(gpu_exec_time/gpu_counts/1_000_000)")
-    println("CPU executions: $(cpu_counts), average CPU execution time $(cpu_exec_time/cpu_counts/1_000_000)")
-
-    return adj_mat
-end
-
-
-function main()
-    n_nodes = 50000
-    
-    p = 0.5
-
-    println("starting precompile run")
-    compute_uniform_random_graph_PZER(5, p)
-
-    println("precompile complete")
-
-
-    compute_uniform_random_graph_PZER(n_nodes, p)
-
-    return
-    display(CUDA.@profile compute_uniform_random_graph_PZER(n_nodes, p))
-    @time compute_uniform_random_graph_PZER(n_nodes, p) 
-    res = zeros(Int, n_nodes, n_nodes)
-    trials = 1
-    
-
-    for i in 1:trials
-        res .+= compute_uniform_random_graph_PZER(n_nodes, p)   
+    if verbose
+        @info "GPU and CPU loop finished"
     end
 
-    emp_mean_edges = sum(res)/trials
-
-
-    anal_mean_edges = p*n_nodes*(n_nodes-1)
-
-    edge_prob_mat = res./trials
-
-    println("edge_prob_mat")
-    println("max:   ", maximum(edge_prob_mat))
-    println("mean: ", mean(edge_prob_mat))
-    println("min:   ", minimum(edge_prob_mat))
-
-    #display(res./trials)
-    println("anal mean edges ", anal_mean_edges)
-    println("emp mean edges ", emp_mean_edges)
-
-
+    average_gpu_exec_time = gpu_exec_time/gpu_counts
+    average_cpu_exec_time = cpu_exec_time/cpu_counts
+    
+    if verbose
+        println("Total execution time: $(total_execution_time/1_000_000) seconds")
+        println("Network dtype: $(typeof(network))")
+        println("N nodes: $(n_nodes)")
+        println("Edge Probability $(edge_probability)")
+        println("Chunksize: $(gpu_chunksize)")
+        println("Total GPU execution time: $(gpu_exec_time/1_000_000) seconds")
+        println("Total CPU execution time: $(cpu_exec_time/1_000_000) seconds")
+        println("GPU executions: $(gpu_counts), average GPU execution time $(average_gpu_exec_time/1_000_000)")
+        println("CPU executions: $(cpu_counts), average CPU execution time $(average_cpu_exec_time/1_000_000)")
+        println("Factor: GPU/CPU $(average_gpu_exec_time/average_cpu_exec_time)")
+    end
 
 end
 
-main()  
 
+function sample_uniform_random_graph_PZER(
+    n_nodes::Integer, 
+    edge_probability::Real;
+    network_dtype::typeof(SimpleNetwork)=infer_space_optimal_network_data_type(n_nodes, round(UInt64, edge_probability*n_nodes*(n_nodes-1)/2)),
+    gpu_chunksize::Integer=1024, 
+    buffersize::Integer=true,
+    verbose=true
+    )
+    
+    if network_dtype == AdjacencyMatrix
+        network = AdjacencyMatrix(falses((n_nodes, n_nodes)))
+    elseif network_dtype == NeighborList
+        network = NeighborList(n_nodes)
+    else
+        throw(Error("Unknown network dtype"))
+    end
+    
+    sample_uniform_random_graph_PZER!(
+        network,
+        n_nodes, 
+        edge_probability;  
+        gpu_chunksize=gpu_chunksize,
+        verbose=verbose
+        )
 
+        return network
+end
 
 
 
--- fastnetworkgeneration/uniformRandomNetworks.jl
+++ fastnetworkgeneration/uniformRandomNetworks.jl
@@ -10,11 +10,36 @@ include("networkDatatypes.jl")
 using .NetworkDataTypes
 
 export generate_uniform_random_graph_geometric
+export sample_uniform_random_graph_PZER
 export connect_isolates!
 export NeighborList, AdjacencyMatrix, SimpleNetwork
 export to_hdf5
 
 include("sampling_algorithms/geometric.jl")
+include("sampling_algorithms/PZER.jl")
 
 
 end
+
+using .UniformRandomNetworks
+
+function main()
+    n_nodes = 5000
+    
+    p = 0.5
+        
+    chunksize = 1024*64
+
+    
+    @info "Starting precompile"
+    sample_uniform_random_graph_PZER(5, p; gpu_chunksize=chunksize, verbose=false)
+
+
+
+    sample_uniform_random_graph_PZER(n_nodes, p, gpu_chunksize=chunksize; verbose=true)
+
+    return
+
+end
+
+main()
