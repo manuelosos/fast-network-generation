0f920c2e9ec22fa283304be226079c3c4d9018f0
MUrban <manuel.urban@uni-bayreuth.de>
2025-07-11 16:54:25 +0200

    added alternative computation of existing edges via directly computing edge coordinates on the gpu.

--- fastnetworkgeneration/sampling_algorithms/PZER.jl
+++ fastnetworkgeneration/sampling_algorithms/PZER.jl
@@ -2,8 +2,120 @@ using CUDA
 using Statistics
 
 
-function compute_single_edge_skip(edge_probability::Float64)
+function compute_single_edge_skip(edge_probability::Float64)::Int
     return max(1, ceil(log(1-edge_probability, rand(Float64))))
+end
+
+
+function linear_to_uppertriagonal_index(n::Int, k::Int, max_edge_index::Int)
+    k = min(k, max_edge_index-1) # Avoids Domainerrors caused by negative values under the squareroor
+    i::Int = n - 2 - floor(Int, sqrt(-8*k + 4*n*(n-1)-7)/2 - 0.5)
+    j::Int = k + i + 1 - (n*(n-1) ÷ 2) +(n-i)*((n-i) - 1) ÷ 2  
+    return (i, j)
+end
+
+
+function compute_edge_coordinates_gpu(
+    n_skips::Int,
+    n_nodes::Int,
+    edge_probability::Real,
+    start_index::Int
+)
+    skip_values = CuArray{Float64}(undef, n_skips)
+    rng_gen(_) = compute_single_edge_skip(edge_probability)
+    skip_values = CUDA.map(rng_gen, skip_values)   
+    skip_values = CUDA.accumulate(+, skip_values)
+
+    skip_values .+= start_index 
+
+    lin2utri(k) = linear_to_uppertriagonal_index(n_nodes, k, convert(Int,n_nodes*(n_nodes-1)/2))    
+    edge_coordinates = CUDA.map(lin2utri, skip_values)    
+
+    
+    return edge_coordinates 
+end
+
+
+function gpu_edge_coordinates_loop(
+    buffer::Channel,
+    n_nodes::Integer,
+    edge_probability::Real,
+    chunk_size::Integer
+)
+
+    @debug "Starting GPU loop"
+    last_edge_index = 0
+    execution_times = 0
+    execution_counts = 0
+
+    max_edges = n_nodes*(n_nodes-1)/2
+
+    while true
+
+        start_time = time()
+        edge_coordinates_d = compute_edge_coordinates_gpu(chunk_size, n_nodes, edge_probability, last_edge_index)        
+        edge_coordinates_h = Array(edge_coordinates_d)
+        @debug "GPU creation time for edge coordinate list $((time()-start_time)/1_000_000)"
+
+        memory_start_time = time()
+        put!(buffer, edge_coordinates_h)
+        @debug "GPU execution time for memory swapping $((time()-memory_start_time)/1_000_000)"
+        
+
+        execution_times  += time() - start_time
+        execution_counts += 1
+
+
+        last_edge_index = convert(Int, edge_coordinates_h[end][1] * n_nodes + edge_coordinates_h[end][2])
+        if last_edge_index > max_edges 
+            close(buffer)
+            return execution_counts, execution_times
+            break
+        end
+
+    end
+
+    return execution_counts, execution_times
+
+end
+
+
+function cpu_edge_coordinates_loop!(
+    network::SimpleNetwork,
+    buffer::Channel,
+    n_nodes::Int;
+)
+    @debug "Starting CPU loop"
+
+    max_n_edges = n_nodes * (n_nodes-1) / 2
+    
+    execution_times = 0
+    execution_counts = 0
+
+    for edge_list in buffer
+        start_time = time()
+        for edge_coordinate in edge_list
+
+            # break condition
+            if edge_coordinate[2] > n_nodes
+                break
+            end
+
+            insert_edge!(network, (edge_coordinate[1], edge_coordinate[2]))
+        end
+
+        execution_counts += 1
+        execution_times += time() - start_time
+
+        @debug "CPU decoding time for edge list: $((time()-start_time)/1_000_000)"
+
+        if row_index >= n_nodes
+            break
+        end
+
+    end
+    @debug "Finishing CPU loop"
+    return execution_counts, execution_times
 end
 
 
@@ -18,7 +130,7 @@ function compute_edge_skips_gpu(
     skip_values = CUDA.accumulate(+, skip_values)
 
     skip_values .+= start_index 
-
+    
     return skip_values
 end
 
@@ -54,12 +166,12 @@ function gpu_compute_loop(
         execution_times  += time() - start_time
         execution_counts += 1
         if edge_list_h[end] > max_edges 
-            @debug "Finishing GPU loop"
             close(buffer)
             return execution_counts, execution_times
             break
         end
     end
+
     return execution_counts, execution_times
 end
 
@@ -125,13 +237,16 @@ function sample_uniform_random_graph_PZER!(
     verbose=true
     )
     
-    buffer = Channel{Vector{Integer}}(buffersize)
+    #buffer = Channel{Vector{Integer}}(buffersize)
+    buffer = Channel{Vector{Tuple{Integer, Integer}}}(buffersize)
 
     start_time = time()
     local gpu_task, cpu_task
     begin 
-        gpu_task = Base.Threads.@spawn gpu_compute_loop(buffer, n_nodes, edge_probability, gpu_chunksize)
-        cpu_task = Base.Threads.@spawn cpu_compute_loop!(network, buffer, n_nodes)
+        #gpu_task = Base.Threads.@spawn gpu_compute_loop(buffer, n_nodes, edge_probability, gpu_chunksize)
+        #cpu_task = Base.Threads.@spawn cpu_compute_loop!(network, buffer, n_nodes)
+        gpu_task = Base.Threads.@spawn gpu_edge_coordinates_loop(buffer, n_nodes, edge_probability, gpu_chunksize)
+        cpu_task = Base.Threads.@spawn cpu_edge_coordinates_loop!(network, buffer, n_nodes)
     end
     total_execution_time = time() - start_time
     gpu_counts, gpu_exec_time = fetch(gpu_task)
--- fastnetworkgeneration/uniformRandomNetworks.jl
+++ fastnetworkgeneration/uniformRandomNetworks.jl
@@ -15,6 +15,8 @@ export connect_isolates!
 export NeighborList, AdjacencyMatrix, SimpleNetwork
 export to_hdf5
 
+export compute_edge_skips_gpu, compute_edge_coordinates_gpu, linear_to_uppertriagonal_index
+
 include("sampling_algorithms/geometric.jl")
 include("sampling_algorithms/PZER.jl")
 
@@ -22,3 +24,63 @@ include("sampling_algorithms/PZER.jl")
 end
 
 
+using .UniformRandomNetworks
+function test_new_gpu_alg()
+
+    
+    chunk_size = 64*1024
+    edge_probability = 0.5
+    n_nodes = 10000
+
+    println("Starting precompile runs")
+    compute_edge_skips_gpu(1024, edge_probability, 1)
+    compute_edge_coordinates_gpu(1024, n_nodes, edge_probability, 1)
+    println("finished precompile")
+
+    println("Starting runs")
+
+
+    execution_time = 0
+
+    n_trials = 100
+
+    for i =1:n_trials
+        start_time = time()
+        compute_edge_skips_gpu(chunk_size, edge_probability, 1)
+        execution_time += time() - start_time 
+    end
+
+
+
+    #start_time_edge_coordinates = time()
+    #compute_edge_coordinates_gpu(chunk_size, n_nodes, edge_probability, 1)
+    #elapsed_time_edge_coordinates = time() - start_time_edge_coordinates
+
+
+
+
+    println("edge")
+    println("Chunksize: $(chunk_size)")
+    println("Estimated nodesize $(sqrt(2*chunk_size))")
+    println("Average execution time after $(n_trials) runs: $(execution_time/n_trials)")
+    #println("Edge skips: ", elapsed_time_edge_skips)
+    #println("Edge_coordinates: ", elapsed_time_edge_coordinates)
+    
+end
+
+
+function test_new_alg()
+
+    chunk_size = 64*1024
+    edge_probability = 0.5
+    n_nodes = 1000000
+
+    print("Starting precompile")
+    generate_uniform_random_graph_PZER(10, 0.5, gpu_chunksize=1024)
+    
+    
+    generate_uniform_random_graph_PZER(n_nodes, edge_probability; gpu_chunksize=chunk_size)
+
+end
+
+test_new_gpu_alg()
